ec71e16601efbbd4229ee333b53fead104f32e56
Paul Dequidt <pdequidt@hotmail.com>
2024-10-10 20:21:55 +0200

    End of Section 1 bis

--- Reactivities/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Reactivities/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45007a0c380265848c4344715e8adf42719be13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3029e399842391a81daee241dc01605b640c2742")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
