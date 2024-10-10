3029e399842391a81daee241dc01605b640c2742
Paul Dequidt <pdequidt@hotmail.com>
2024-10-10 20:18:35 +0200

    End of Section 1

--- Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Reactivities/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45007a0c380265848c4344715e8adf42719be13")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
